20421e3b6cd497c7a346d3eb7a61c814e0ed78cc
jaythestampede <jameswboyd32@gmail.com>
2023-06-30 08:27:50 -0400

    added route files

--- MERN/server.js
+++ MERN/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
+
 
 const PORT = process.env.PORT || 3000;
 
