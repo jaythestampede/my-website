8505774d0a1558ed95499d99e13e1055144b849c
jaythestampede <jameswboyd32@gmail.com>
2023-07-08 22:55:56 -0400

    routes with files

--- MERN-2/server/index.js
+++ MERN-2/server/index.js
@@ -8,6 +8,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +35,9 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+/*  ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
@@ -44,4 +48,4 @@ mongoose
 .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 })
-.catch((error) => console.log(`${error} did not connect`))
+.catch((error) => console.log(`${error} did not connect`));
