3514b3107004d6e63d6c22e09ecf66ff11940f6d
jaythestampede <jameswboyd32@gmail.com>
2023-08-05 21:49:40 -0400

    post routes setup

--- MERN-2/server/index.js
+++ MERN-2/server/index.js
@@ -9,8 +9,11 @@ import helmet from "helmet";
 import path from "path";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
+import postRoutes from "./routes/post.js";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
+import { register } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -39,10 +42,12 @@ const upload = multer({ storage });
 
 /*  ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
-app._router("/auth", authRoutes);
-app._router("/users", userRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 
 /* MONGOOSE SETUP */
