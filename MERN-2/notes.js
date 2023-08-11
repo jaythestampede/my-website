98f6bc30c833fb276a60d27a2b09d9598754e16f
jaythestampede <jameswboyd32@gmail.com>
2023-08-11 14:21:52 -0400

    notes

---
+++ MERN-2/notes.js
@@ -0,0 +1,13 @@
+/* TIMESTAMPS 
+currently on 1:48:29 (8-11-2023)
+taking a break from the entire project to work on mini projects
+    in order to better understand the backend setup
+
+Current Blockers
+- MongoDB Setup is not working, I need better understanding of databases
+-React modules
+-Overall backend setup is confusing, I thonk I built the APIs via paths and controllers
+     but I cannot currently speak on how or what is going on.
+
+
+*/
